eb82f0e9217c0e1c0d1e870c903399e0c95a6f85
Dariia <dariia.kucheruk@ukma.edu.ua>
2021-05-09 16:29:09 +0300

    to use function from other package you have to specify package before function name (ex: package.function())

--- testForGitHub/run.go
+++ testForGitHub/run.go
@@ -1,8 +1,13 @@
 package main
 
 import "fmt"
+import (
+	"DBsummer/newDirectoryForSecondCommit"
+)
 
 func main() {
 	fmt.Println("Hello world!")
 	fmt.Println("This is my second commit")
+
+	newDirectoryForSecondCommit.PrintStuff()
 }
