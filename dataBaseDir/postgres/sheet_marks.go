049b910abbbea3d5c5ecf48bcf80b62f440f781c
Dariia <dariia.kucheruk@ukma.edu.ua>
2021-06-07 21:17:07 +0300

    upd

--- dataBaseDir/postgres/sheet_marks.go
+++ dataBaseDir/postgres/sheet_marks.go
@@ -1,6 +1,10 @@
 package postgres
 
-import "context"
+import (
+	"DBsummer/pdfReading"
+	"context"
+	"math/rand"
+)
 
 type SheetMarksRepository struct {
 	*Repository
@@ -12,4 +16,33 @@ func (r SheetMarksRepository) Create(ctx context.Context) (id int, err error) {
 
 func (r SheetMarksRepository) Get(ctx context.Context) error {
 	panic("implement me")
+}
+
+func (r SheetMarksRepository) PostSheetMarksToDataBase(ctx context.Context, sheetID int, sheetMarks *pdfReading.StudInfoFromPDF) error {
+	getStudentCipher := r.db.Rebind(`
+		SELECT student_cipher
+		FROM student
+		WHERE firstname = ? AND last_name = ? AND (middle_name = ? OR middle_name IS NULL OR middle_name = '')
+;
+	`)
+	row := r.db.QueryRowContext(ctx, getStudentCipher, sheetMarks.FirstName, sheetMarks.Lastname, sheetMarks.MiddleName)
+	var studentID string
+	err := row.Scan(&studentID)
+	if err != nil {
+		return err
+	}
+
+	query := r.db.Rebind(`
+		INSERT into sheet_marks(check_mark,mark_number,national_mark,semester_mark,
+	together_mark,ects_mark,sheet,student) VALUES(?,?,?,?,?,?,?,?);
+		`)
+
+	id := int(rand.Uint32())
+
+	_, err = r.db.Exec(query, sheetMarks.ControlMark, id, sheetMarks.NationalMark, sheetMarks.SemesterMark, sheetMarks.TogetherMark,
+		sheetMarks.EctsMark, sheetID, studentID)
+	if err != nil {
+		return err
+	}
+	return nil
 }
